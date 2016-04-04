b9303ea633aecbde41a891bc248e26c72020a0c9
Alan Kenyon <akenyon@dstcontrols.com>
2016-04-04 12:21:22 -0700

    basic dashboard

--- tabletDashboardWizard/tabletDashboardWizard/platforms/android/assets/www/js/packeryDirectives.js
+++ tabletDashboardWizard/tabletDashboardWizard/platforms/android/assets/www/js/packeryDirectives.js
@@ -1,6 +1,6 @@
 ﻿angular.module('packery.controllers', [])
 
-.directive('packeryWorkspace', function () {
+.directive('packeryWorkspace', function ($compile) {
     return {
         scope: {
             configuration: "="
@@ -8,10 +8,35 @@
         restrict: 'A',
         link: function ($scope, element, attributes) {
 
-
-            angular.forEach($scope.configuration.Widgets, function (widget) {
-                var angularElement = 
+            element.ready(function () {
+                $scope.packery = new Packery(element[0], {
+                    rowHeight: 100,
+                    itemSelector: '.module',
+                    columnWidth: 100
+                });
+                angular.forEach($scope.packery.getItemElements(), function (item) {
+                    var draggable = new Draggabilly(item);
+                    $scope.packery.bindDraggabillyEvents(draggable);
+                });
+                $scope.packery.layout();
             });
+
+            for (var i = 0; i < $scope.configuration.Widgets.length; i++) {
+                var widgetElement = angular.element("<div class='module'>configuration.Widgets[" + i + "]</div>");
+                $compile(widgetElement)($scope);
+                element.append(widgetElement);
+                $compile(widgetElement)($scope);
+            }
+        }
+    }
+})
+.directive('packeryContainer', function ($compile) {
+    return {
+        scope : {
+            configuration: '='
+        },
+        restrict: 'A',
+        link: function ($scope, element, attributes) {
 
         }
     }
