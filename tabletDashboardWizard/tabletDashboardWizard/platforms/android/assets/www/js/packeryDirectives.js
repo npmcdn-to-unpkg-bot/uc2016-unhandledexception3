a1200a47ab5377bf06193eb4657617bbc094968d
Alan Kenyon <akenyon@dstcontrols.com>
2016-04-04 11:57:26 -0700

    pushing more shit

---
+++ tabletDashboardWizard/tabletDashboardWizard/platforms/android/assets/www/js/packeryDirectives.js
@@ -0,0 +1,18 @@
+﻿angular.module('packery.controllers', [])
+
+.directive('packeryWorkspace', function () {
+    return {
+        scope: {
+            configuration: "="
+        },
+        restrict: 'A',
+        link: function ($scope, element, attributes) {
+
+
+            angular.forEach($scope.configuration.Widgets, function (widget) {
+                var angularElement = 
+            });
+
+        }
+    }
+})
