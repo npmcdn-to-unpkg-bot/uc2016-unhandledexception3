04716ecbf6c28c80d80a97d13da6a09260eee96f
Sarven Buyuker <sbuyuker@dstcontrols.com>
2016-04-04 19:40:34 -0700

    commit

---
+++ tabletDashboardWizard/tabletDashboardWizard/platforms/ios/www/js/lovely.js
@@ -0,0 +1,14 @@
+﻿angular.module('lovely', [])
+.factory('lovelyDataService', ['$http', function ($http) {
+    var endpoint = 'http://callitwhateveryoulike.azurewebsites.net/lovely/';
+
+    var getKpi = function (webId) {
+        return $http.get(endpoint + 'GetKPI?_WebId=' + webId);
+    }
+    
+
+
+    return {
+        getKpi: getKpi
+    }
+}])
---
+++ tabletDashboardWizard/tabletDashboardWizard/platforms/ios/www/js/packeryDirectives.js
@@ -0,0 +1,55 @@
+﻿angular.module('packery.controllers', ['widget'])
+
+.directive('packeryWorkspace', function ($compile) {
+    return {
+        scope: {
+            configuration: "="
+        },
+        restrict: 'A',
+        transclude: true,
+        link: function ($scope, element, attributes) {
+
+            element.ready(function () {
+                $scope.packery = new Packery(element[0], {
+                    rowHeight: 100,
+                    itemSelector: '.module',
+                    columnWidth: 100
+                });
+                angular.forEach($scope.packery.getItemElements(), function (item) {
+                    var draggable = new Draggabilly(item);
+                    $scope.packery.bindDraggabillyEvents(draggable);
+                });
+                $scope.packery.layout();
+            });
+
+            for (var i = 0; i < $scope.configuration.Widgets.length; i++) {
+                var widgetElement = angular.element("<div packery-Container configuration='configuration.Widgets[" + i + "]' class='module'></div>");
+                $compile(widgetElement)($scope);
+                element.append(widgetElement);
+            }
+        }
+    }
+})
+.directive('packeryContainer', function ($compile) {
+
+    return {
+        scope : {
+            configuration: '='
+        },
+        restrict: 'A',
+        transclude: true,
+        link: function ($scope, element, attributes) {
+            var container = $scope.configuration.container;
+            var widget = $scope.configuration.widget;
+
+            //set container size
+            element.addClass("width-" + container.width);
+            element.addClass("height-" + container.height);
+
+            //create and compile widget
+            var widgetElement = angular.element("<" + widget.type + " config='configuration.widget'></" + widget.type + ">");
+            $compile(widgetElement)($scope);
+            element.append(widgetElement);
+        }
+    }
+})
